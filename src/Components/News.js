b6f3a69431a8b67e97fbd210a5bbbc3595649ad8
Abhishek Patidar <abhipatidar253@gmail.com>
2024-12-17 19:51:39 +0530

    Using API to fetch Dynamic content v27

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,95 +2,21 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  //Data members shoudld be defined here
-  articles = [
-      {
-        "source": {
-            "id": "australian-financial-review",
-            "name": "Australian Financial Review"
-        },
-        "author": "Gus McCubbing",
-        "title": "Victorian politics: Labor’s Tim Pallas resigns after 10 years as treasurer",
-        "description": "The state treasurer says Labor’s ambitions were always bigger than the budget would allow, but he “never” questioned the $100 billion Suburban Rail Loop.",
-        "url": "http://www.afr.com/politics/my-greatest-regret-pallas-resigns-as-victoria-s-debt-grows-20241216-p5kyni",
-        "urlToImage": "https://static.ffx.io/images/$zoom_0.3646%2C$multiply_3%2C$ratio_1.777778%2C$width_1059%2C$x_180%2C$y_107/t_crop_custom/c_scale%2Cw_800%2Cq_88%2Cf_jpg/t_afr_no_label_no_age_social_wm/e25aff622ba219864b1ea91afe2aa783cbac4e80",
-        "publishedAt": "2024-12-16T02:36:17Z",
-        "content": "Victorias Treasurer Tim Pallas says his one major regret after a decade in the role and nearly 20 years in parliament is that Labors ambitions were bigger than its purse strings. But he has no qualms… [+384 chars]"
-    },
-    {
-        "source": {
-            "id": "politico",
-            "name": "Politico"
-        },
-        "author": null,
-        "title": "The nation’s cartoonists on the week in politics",
-        "description": "Every week political cartoonists throughout the country and across the political spectrum apply their ink-stained skills to capture the foibles, memes, hypocrisies and other head-slapping events in the world of politics. The fruits of these labors are hundred…",
-        "url": "https://www.politico.com/gallery/2024/12/13/the-nations-cartoonists-on-the-week-in-politics-00194162",
-        "urlToImage": "https://static.politico.com/5e/7f/66c3d8e94a48bf2b57401cfd516c/0-teaser.jpg",
-        "publishedAt": "2024-12-13T13:27:56+00:00",
-        "content": "Cartoon Carousel Every week political cartoonists throughout the country and across the political spectrum apply their ink-stained skills to capture the foibles, memes, hypocrisies and other head-s… [+260 chars]"
-    },
-    {
-        "source": {
-            "id": "politico",
-            "name": "Politico"
-        },
-        "author": "Megan Messerly",
-        "title": "‘Now she’s the world’s problem’: Some Arizona Republicans relieved Lake is heading to Washington - POLITICO",
-        "description": "She's still expected to play a role in Arizona politics.",
-        "url": "https://www.politico.com/news/2024/12/12/kari-lake-voa-republicans-00194096",
-        "urlToImage": "https://static.politico.com/4c/a4/11d34dff49a39ee4ecd66021d84a/election-2024-rnc-52916.jpg",
-        "publishedAt": "2024-12-12T21:31:37+00:00",
-        "content": "When youre a two-time loser and you did worse the second time around, thats a clue that maybe the voters dont want you, said Barrett Marson, a GOP strategist in Arizona. I think it clears space for t… [+4416 chars]"
-    },
-    {
-        "source": {
-            "id": "cnn",
-            "name": "CNN"
-        },
-        "author": "Dawn Handley",
-        "title": "CNN Headlines | CNN",
-        "description": "CNN Headlines is a curated channel covering major news events across politics, international, business, and entertainment, and showcasing the most impactful stories of the day.",
-        "url": "https://www.cnn.com/videos/fast/cnn-headlines",
-        "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/cnn-headlines-placeholder-1920x1080.png?c=16x9&q=w_800,c_fill",
-        "publishedAt": "2024-09-03T19:55:38.781Z",
-        "content": "CNN Headlines is a curated channel covering major news events across politics, international, business, and entertainment, and showcasing the most impactful stories of the day."
-    },
-    {
-        "source": {
-            "id": "the-american-conservative",
-            "name": "The American Conservative"
-        },
-        "author": null,
-        "title": "Politics Archives - The American Conservative",
-        "description": null,
-        "url": "https://www.theamericanconservative.com/category/politics/",
-        "urlToImage": null,
-        "publishedAt": "2022-07-07T21:37:27.3936289Z",
-        "content": null
-    },
-    {
-        "source": {
-            "id": "the-jerusalem-post",
-            "name": "The Jerusalem Post"
-        },
-        "author": null,
-        "title": "Congresswoman Nita Lowey: I am proud to stand with Israel",
-        "description": "Gantz: Security of Israel is above politics; PA: This is a crime.",
-        "url": "https://www.jpost.com/Arab-Israeli-Conflict/Gantz-Security-of-Israel-is-above-politics-Palestinians-This-is-a-crime-607595",
-        "urlToImage": "https://images.jpost.com/image/upload/f_auto,fl_lossy/t_Article2016_ControlFaceDetect/448812",
-        "publishedAt": "2019-11-13T04:41:00Z",
-        "content": "US Ambassador David M. Friedman said the US stands “with our friend and ally Israel at this critical moment” on social media on Tuesday after roughly 170 rockets were fired on Israel from the Gaza St… [+6160 chars]"
-    }
-  ]
   
   constructor(){      //constructor called whenever we create an object
     super();          //It should always here..
-    console.log("Hello I am a constructor form news compo");
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false      //we will use it whenever page will load and we want to show some loading Icon
     }
+  }
+
+  async componentDidMount(){    //this cdm runs after the render() which is below
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=028c82e1117c4615a152c8c3862e1e4c";
+    let data = await fetch(url);
+    let parsedData = await data.json();   //await: to wait for the url to load content
+    // console.log(parsedData);
+    this.setState({articles: parsedData.articles})
   }
   
   render() {
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: '18rem'}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={imageUrl?imageUrl:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi_bMyK51UEk6G7B-Umw22ugEyKzM5XopsWrKqUMglTqQRoEKUlvK08LNAHhRi6Vlso1TgpglAzpIe7x9KD2Tyt6HOkYIRMwpYPaW2tdEDifYO85t8r7GQCJfUum_QbH59dbdvseZ4Wt0BxKzh_j-Zxz7oVxWrnPDH6uVfmsLd_PQhPOxUw4H7nqsPSiFw/s728-rw-e365/threathunting.png"} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}..</h5>
             <p className="card-text">{description}..</p>
