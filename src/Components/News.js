807cb7f0f50b16645cdc5bb185672645b2f88722
Abhishek Patidar <abhipatidar253@gmail.com>
2024-12-18 19:01:26 +0530

    Adding Categories & propTypes v30

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+  
+  static defaultProps = { //syntax is there in documentation
+    country: 'us',
+    pageSize: 9,
+    category: 'general'  //this is default category taken from newsAPI plateform.
+  }
+
+  static propTypes = {    //syntax is there in documentation
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
   
   constructor(){
     super();
@@ -14,7 +27,7 @@ export class News extends Component {
   }
 
   async componentDidMount(){ 
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=1`; //here in class based component no need to pass props as argument becoz classes never takes arg/parameter as funtions
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=1`; //here in class based component no need to pass props as argument becoz classes never takes arg/parameter as funtions
     this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -27,7 +40,7 @@ export class News extends Component {
 
   handlePrevClick = async ()=>{
     console.log("prev");
-      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=${this.state.page - 1}`; //setting pageSize varible by giving props to the Newj component
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=${this.state.page - 1}`; //setting pageSize varible by giving props to the Newj component
       this.setState({loading: true})
       let data = await fetch(url);
       let parsedData = await data.json();
@@ -42,7 +55,7 @@ export class News extends Component {
     console.log("next");
     if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){  //21 is replaced by this.props.pageSize everywhere
         //negation is applied so we can do else vala part in it
-      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=${this.state.page + 1}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=${this.state.page + 1}`;
       this.setState({loading: true});
       let data = await fetch(url);
       let parsedData = await data.json();
@@ -57,7 +70,7 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h2 className='text-center'>NewsThreads Tranding Topics</h2>
+        <h2 className='text-center' style={{margin: '35px 0px'}}>NewsThreads Tranding Topics</h2>
         {this.state.loading && <Spinner/>}
         <div className="row">
           {!this.state.loading && this.state.articles.map((element)=>{
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     
     return (
       <div className='my-3'>
-        <div className="card" style={{width: '18rem'}}>
+        <div className="card">
           <img src={imageUrl?imageUrl:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi_bMyK51UEk6G7B-Umw22ugEyKzM5XopsWrKqUMglTqQRoEKUlvK08LNAHhRi6Vlso1TgpglAzpIe7x9KD2Tyt6HOkYIRMwpYPaW2tdEDifYO85t8r7GQCJfUum_QbH59dbdvseZ4Wt0BxKzh_j-Zxz7oVxWrnPDH6uVfmsLd_PQhPOxUw4H7nqsPSiFw/s728-rw-e365/threathunting.png"} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}..</h5>
