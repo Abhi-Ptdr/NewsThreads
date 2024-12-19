1aebbd1a999d548c64608fcb66da3e2fa1422365
Abhishek Patidar <abhipatidar253@gmail.com>
2024-12-19 09:19:43 +0530

    Adding author, Date, NewsSource v32

--- src/Components/News.js
+++ src/Components/News.js
@@ -22,12 +22,12 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page:1
+      page: 1
     }
   }
 
   async componentDidMount(){ 
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=1`; //here in class based component no need to pass props as argument becoz classes never takes arg/parameter as funtions
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&pageSize=${this.props.pageSize}&apiKey=028c82e1117c4615a152c8c3862e1e4c&page=1`; //here in class based component no need to pass props as argument becoz classes never takes arg/parameter as funtions
     this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -40,7 +40,7 @@ export class News extends Component {
 
   handlePrevClick = async ()=>{
     console.log("prev");
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=${this.state.page - 1}`; //setting pageSize varible by giving props to the Newj component
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&pageSize=${this.props.pageSize}&apiKey=028c82e1117c4615a152c8c3862e1e4c&page=${this.state.page - 1}`; //setting pageSize varible by giving props to the Newj component
       this.setState({loading: true})
       let data = await fetch(url);
       let parsedData = await data.json();
@@ -55,7 +55,7 @@ export class News extends Component {
     console.log("next");
     if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){  //21 is replaced by this.props.pageSize everywhere
         //negation is applied so we can do else vala part in it
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=028c82e1117c4615a152c8c3862e1e4c&pageSize=${this.props.pageSize}&page=${this.state.page + 1}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&pageSize=${this.props.pageSize}&apiKey=028c82e1117c4615a152c8c3862e1e4c&page=${this.state.page + 1}`;
       this.setState({loading: true});
       let data = await fetch(url);
       let parsedData = await data.json();
@@ -75,7 +75,8 @@ export class News extends Component {
         <div className="row">
           {!this.state.loading && this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title?.slice(0, 45) || "Title not available"} description={element.description?.slice(0, 80) || "Description not available"} newsUrl={element.url} imageUrl={element.urlToImage}/>
+              <NewsItem title={element.title?.slice(0, 45) || "Title not available"} description={element.description?.slice(0, 80) || "Description not available"} 
+              newsUrl={element.url} imageUrl={element.urlToImage} author={element.author} date={element.publishedAt} source={element.source.name}/>
             </div>
           })}
         </div>
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+// import { data } from 'react-router-dom'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props    //array destructuring syntax
     
     return (
       <div className='my-3'>
         <div className="card">
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
           <img src={imageUrl?imageUrl:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi_bMyK51UEk6G7B-Umw22ugEyKzM5XopsWrKqUMglTqQRoEKUlvK08LNAHhRi6Vlso1TgpglAzpIe7x9KD2Tyt6HOkYIRMwpYPaW2tdEDifYO85t8r7GQCJfUum_QbH59dbdvseZ4Wt0BxKzh_j-Zxz7oVxWrnPDH6uVfmsLd_PQhPOxUw4H7nqsPSiFw/s728-rw-e365/threathunting.png"} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}..</h5>
             <p className="card-text">{description}..</p>
+            <p className="card-text"><small className="text-body-secondary">By {author?author:"Unknown"} on {new Date(date).toGMTString()} </small></p>
             <a className="btn btn-dark btn-sm" href={newsUrl} target="_blank" rel="noreferrer">Read More</a> {/*target="_blank" to open link in new tab*/}
           </div>
         </div>
@@ -20,3 +23,22 @@ export class NewsItem extends Component {
 }
 
 export default NewsItem
+
+
+// NOTE: 
+// "publishedAt": "2024-12-16T02:36:17Z" we have this "ISO" string in our fetched data
+
+// so we can use so many javascript methods on it for eg.
+
+// const a = "2024-12-16T02:36:17Z";
+// const date = new Date(a);
+// const year = date.getUTCFullYear(); // 2024
+// const month = date.getUTCMonth() + 1; // 12 (0-indexed)
+// const day = date.getUTCDate(); // 16
+// const hours = date.getUTCHours(); // 2
+// const minutes = date.getUTCMinutes(); // 36
+// const seconds = date.getUTCSeconds(); // 17
+// const gmtString = date.toUTCString(); // "Mon, 16 Dec 2024 02:36:17 GMT"
+// const onlyDate = date.toISOString().split('T')[0]; // "2024-12-16"
+// const onlyTime = date.toISOString().split('T')[1].split('Z')[0]; // "02:36:17"
+
